012c4b2fc67bb266744bc944f24322be4e23673c
wsl@root <888iee@github.com>
2020-05-01 15:10:38 +0200

    added token authorization

--- src/includes/signup_includes.php
+++ src/includes/signup_includes.php
@@ -31,50 +31,83 @@ if (isset($_POST['signup-submit'])) {
         exit();
     // db stuff
     } else {
-        $sql = "SELECT uid FROM users WHERE uid=?;";
-        $stmt = mysqli_stmt_init($conn);
-        // check if valid input
-        if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../signup.php?error=sqlerror");
+        // check for token
+        $sqlToken   = "SELECT * FROM auth WHERE token=?;";
+        $stmtToken  = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmtToken, $sqlToken)) {
+            header("Location: ../signup.php?error=sqlerror&id=".$id);
             exit();
         } else {
             // post value type
-            mysqli_stmt_bind_param($stmt, "s", $id);
-            // execute statement in db
-            mysqli_stmt_execute($stmt);
-            // save result in $stmt
-            mysqli_stmt_store_result($stmt);
-            // get number of rows of result
-            $resultCheck = mysqli_stmt_num_rows($stmt);
-            // check if username exist
-            if ($resultCheck > 0) {
-                header("Location: ../signup.php?error=usertaken&id=".$id."&authToken=".$authToken);
-                exit();
-            } else {
-                // if not add to db
-                $sql = "INSERT INTO users (uid, pwd) VALUES(?, ?)";
-                $stmt = mysqli_stmt_init($conn);
-                if (!mysqli_stmt_prepare($stmt, $sql)) {
-                    header("Location: ../signup.php?error=sqlerror");
+            mysqli_stmt_bind_param($stmtToken, "s", $authToken);
+            // find in db
+            mysqli_stmt_execute($stmtToken);
+            // get result
+            $checkResult = mysqli_stmt_get_result($stmtToken);
+            // check if result is null
+            if ($row = mysqli_fetch_assoc($checkResult)) {
+                // check if token is used
+                if($row['used'] == 1)  {
+                    header("Location: ../signup.php?error=tokenused&id=".$id);
                     exit();
                 } else {
-                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-                    // post value type
-                    mysqli_stmt_bind_param($stmt, "ss", $id, $hashedPwd);
-                    // execute statement in db
-                    mysqli_stmt_execute($stmt);
+                    // set tokenid to var
+                    $tokenId = $row['id'];
+                    // check for user
+                    $sql = "SELECT uid FROM users WHERE uid=?;";
+                    $stmt = mysqli_stmt_init($conn);
+                    // check if valid input
+                    if (!mysqli_stmt_prepare($stmt, $sql)) {
+                        header("Location: ../signup.php?error=sqlerror");
+                        exit();
+                    } else {
+                        // post value type
+                        mysqli_stmt_bind_param($stmt, "s", $id);
+                        // execute statement in db
+                        mysqli_stmt_execute($stmt);
+                        // save result in $stmt
+                        mysqli_stmt_store_result($stmt);
+                        // get number of rows of result
+                        $resultCheck = mysqli_stmt_num_rows($stmt);
+                        // check if username exist
+                        if ($resultCheck > 0) {
+                            header("Location: ../signup.php?error=usertaken&id=".$id."&authToken=".$authToken);
+                            exit();
+                        } else {
+                            // if not add to db
+                            $sql = "INSERT INTO users (uid, pwd) VALUES(?, ?)";
+                            $stmt = mysqli_stmt_init($conn);
+                            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                                header("Location: ../signup.php?error=sqlerror");
+                                exit();
+                            } else {
+                                $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+                                // post value type
+                                mysqli_stmt_bind_param($stmt, "ss", $id, $hashedPwd);
+                                // execute statement in db
+                                mysqli_stmt_execute($stmt);
 
-                    // Send user to sign up successful
-                    header("Location: ../signup.php?signup=success");
-                    exit();
-                
+                                $sqlToken = "UPDATE auth SET used=1 WHERE id=".$tokenId;
+                                if(mysqli_query($conn, $sqlToken)) {
+
+                                    // Send user to sign up successful
+                                    header("Location: ../signup.php?signup=success");
+                                    exit();
+                                }
+                            }
+                        }
+                    }
                 }
+            } else {
+                header("Location: ../signup.php?error=invalidtoken&id=".$id);
+                exit();
             }
         }
     }
     
     // delete statement
     mysqli_stmt_close($stmt);
+    mysqli_stmt_close($stmtToken);
     // close db connection
     mysqli_close($conn);
 } else {
