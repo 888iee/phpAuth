02d23eb79e6a52242b647b246fc3bf26140c619e
wsl@root <888iee@github.com>
2020-05-03 01:33:14 +0200

    changed design

--- src/index.php
+++ src/index.php
@@ -9,16 +9,18 @@
             if(isset($_SESSION['ROLE'])) {
                 if($_SESSION['ROLE'] == "ADMIN"){ 
                     echo '<h1 class="notes">Control Panel</h1>';
+                    echo '
+                        <section>
+                            <form action="includes/wake_includes.php" method="post">
+                                <button class="wakebtn" type="submit">
+                                    Wake Server
+                                </button>
+                            </form>
+                        </section>';
                 }
             }
         ?>
-        <section>
-            <form action="includes/wake_includes.php" method="post">
-                <button class="wakebtn" type="submit">
-                    Wake Server
-                </button>
-            </form>
-        </section>
+
 
     </main>
 
