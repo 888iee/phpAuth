d8695ea33ede6e2c926185bb9b37d7a9d5c4edd9
sid@root <sidathwip@aol.com>
2020-07-30 11:39:21 +0200

    bug fix session params couldn't be accessed because session wasn't set for this page

--- src/includes/wake_includes.php
+++ src/includes/wake_includes.php
@@ -1,6 +1,8 @@
-<!-- insert wol script call -->
 <?php
-if(isset($_SESSION['userId'])){
-    system("python wol.py 192.168.178.255 00:19:99:DC:DA:46");
-    header("Location:../index.php");
-}
+    session_start();
+
+    if(isset($_SESSION['userId'])){
+        system("python wol.py 192.168.178.255 00:19:99:DC:DA:46");
+        header("Location:../index.php");
+    }
+?>
